d235b470830bf7042a0e93b29c4814660cef0ce8
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2024-11-20 00:16:35 +0700

    init pokemon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  const getPokemon = async () => {
+    try {
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20");
+      console.log(response);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching Pokemon:", error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    getPokemon()
+      .then(data => setData(data.results))
+      
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="h-screen w-full bg-zinc-800 text-white">
+      <div className="h-56 bg-zinc-600"></div>
+      {data.map((pokemon)=><li class="list-none">
+        {pokemon.name},{pokemon.url}
+      </li>)}
+    </div>
+    
+  );
 }
 
+  
 export default App
