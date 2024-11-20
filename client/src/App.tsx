3fe7e3f10e1b8dfe869a843d150854d39f5d8ca1
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2024-11-21 00:02:36 +0700

    add sprites lol

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,13 +8,17 @@ function App() {
   const getPokemon = async () => {
     try {
       const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20");
-      console.log(response);
+      console.log(response.data.results)
       return response.data;
     } catch (error) {
       console.error("Error fetching Pokemon:", error);
       throw error;
     }
+
+    
   };
+
+  
 
   useEffect(() => {
     getPokemon()
@@ -22,13 +26,25 @@ function App() {
       
   }, []);
 
+ const sprite = (key: number)=>{
+    key+=1
+    let base:string = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+    let index:string = key.toString()
+    return base+index+".png"
+ }
+
   return (
     <div className="min-h-screen w-full bg-zinc-800 text-white">
-      <div className="h-56 bg-zinc-600"></div>
+      <div className="h-56 bg-zinc-600 "></div>
 
-      <div className="bg-black">{data.map((pokemon)=> <div>{pokemon.name}</div> )}</div>
+      <div className="bg- flex flex-wrap flex-row gap-72 p-20 justify-center items-center">
+        {data.map((pokemon,index)=> <div key={index} className="flex justify-center items-center">{pokemon.name}
+      <img src={sprite(index)} alt={pokemon.name}>
+
+      </img></div>)}</div>
     </div>
     
+
   );
 }
 
